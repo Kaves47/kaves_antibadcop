6a3f0bc8847e3056ce036d3b24b1ed751f58060e
Mustafa <mustafabuyuktas06@gmail.com>
2020-12-20 23:29:48 +0300

    kaves_antibadcop

---
+++ fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version "adamant"
+
+game "gta5"
+
+--[[
+
+BU SCRIPT Kaves#0359 TARAFINDAN YAZILMIŞTIR.
+THIS SCRIPT WAS WRITTEN BY Kaves#0359
+
+]]--
+
+client_scripts {
+    
+    "client.lua",
+
+}
